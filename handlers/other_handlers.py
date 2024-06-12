2e33f1606a6a982b6d7d09cbdd27955904456ea8
antONget <a.l.ponomare@mail.ru>
2024-06-12 12:26:11 +0300

    await get list raffle

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -3,13 +3,13 @@ import asyncio
 from aiogram import Router
 from aiogram.types import Message, CallbackQuery
 from aiogram.types import FSInputFile
-from module.data_base import get_list_user
+from module.data_base import get_list_user, get_list_last_raffle
 from services.get_exel import list_users_to_exel
-
+from config_data.config import Config, load_config
 import logging
 
 router = Router()
-
+config: Config = load_config()
 
 
 
@@ -31,25 +31,40 @@ async def all_message(message: Message) -> None:
         logging.info(f'all_message message.sticker')
         # Получим ID Стикера
         # print(message.sticker.file_id)
-    if message.text == '/get_logfile':
-        file_path = "py_log.log"
-        await message.answer_document(FSInputFile(file_path))
-    if message.text == '/get_dbfile':
-        file_path = "database.db"
-        await message.answer_document(FSInputFile(file_path))
-    if message.text == '/get_listusers':
-        list_user = get_list_user()
-        text = 'Список пользователей:\n'
-        for i, user in enumerate(list_user):
-            text += f'{i+1}. {user[1]} - {user[2]}\n'
-            if i % 10 == 0 and i > 0:
-                await asyncio.sleep(0.2)
-                await message.answer(text=text)
-                text = ''
-        await message.answer(text=text)
-    if message.text == '/get_exel':
-        list_users_to_exel()
-        file_path = "list_user.xlsx"  # или "folder/filename.ext"
-        await message.answer_document(FSInputFile(file_path))
-
+    list_super_admin = list(map(int, config.tg_bot.admin_ids.split(',')))
+    if message.chat.id in list_super_admin:
+        if message.text == '/get_logfile':
+            file_path = "py_log.log"
+            await message.answer_document(FSInputFile(file_path))
+        if message.text == '/get_dbfile':
+            file_path = "database.db"
+            await message.answer_document(FSInputFile(file_path))
+        if message.text == '/get_listusers':
+            list_user = get_list_user()
+            text = 'Список пользователей:\n'
+            for i, user in enumerate(list_user):
+                text += f'{i+1}. {user[1]} - {user[2]}\n'
+                if i % 10 == 0 and i > 0:
+                    await asyncio.sleep(0.2)
+                    await message.answer(text=text)
+                    text = ''
+            await message.answer(text=text)
+        if message.text == '/get_exel':
+            list_users_to_exel()
+            file_path = "list_user.xlsx"  # или "folder/filename.ext"
+            await message.answer_document(FSInputFile(file_path))
+        if '/get_CULR' in message.text:
+            list_message = message.text.split()
+            if len(list_message) == 3:
+                done_task = int(list_message[2])
+                list_raffle = get_list_last_raffle(done_task=done_task)
+                list_raffle_0 = get_list_last_raffle(done_task=0)
+                await message.answer(text=f'Количество участников выполнивших {done_task} заданий'
+                                          f'{len(list_raffle)}\n'
+                                          f'Количество участников зарегистрировавшихся на участие в розыгрыше'
+                                          f'{len(list_raffle_0)}')
+            elif len(list_message) == 2:
+                list_raffle = get_list_last_raffle(done_task=0)
+                await message.answer(text=f'Количество участников зарегистрировавшихся на участие в розыгрыше'
+                                          f'{len(list_raffle)}')
 
